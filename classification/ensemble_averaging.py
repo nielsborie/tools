f0116246aead8bb0f4adac2ffe83298f398c6523
Niels Borie <40639235+nielsborie@users.noreply.github.com>
2018-08-07 03:01:29 +0200

    Create ensemble_averaging.py
    add extension file

---
+++ classification/ensemble_averaging.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn.base import BaseEstimator,RegressorMixin,TransformerMixin
+
+# --- Custom ensemble method : averaging predictions
+class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, models):
+        self.models = models
+        
+    # we define clones of the original models to fit the data in
+    # the reason of clone is avoiding affect the original base models
+    def fit(self, X, y):
+        self.models_ = [clone(x) for x in self.models]  
+        # Train cloned base models
+        for model in self.models_:
+            model.fit(X, y)
+        return self
+    
+    #Now we do the predictions for cloned models and average them
+    def predict(self, X):
+        predictions = np.column_stack([ model.predict(X) for model in self.models_ ])
+        return np.mean(predictions, axis=1)
